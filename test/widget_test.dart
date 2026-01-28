8de6523cdd90690cd4076c808b9b0e5ba5eb6d07
Aiham <aiham.anter.g51249@mcast.edu.mt>
2026-01-28 23:18:43 +0100

    fix widget_test placeholder

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,14 +5,15 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:shelf_life_checker/main.dart';
 
 void main() {
-  test('placeholder test', () {
-    expect(1 + 1, 2);
+  testWidgets('app starts', (tester) async {
+    await tester.pumpWidget(const ShelfLifeCheckerApp());
+    await tester.pump();
+    expect(find.text('Inventory'), findsOneWidget);
   });
 }
+
 
